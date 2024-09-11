4863d316d0425aa4cfabb3f5c61a05bbc681abb6
Senu-sesi2 <senu.sesi2@gmail.com>
2024-09-11 16:33:45 +0000

    create navbar to display search bar, message, and profile-icon

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+const Navbar = () => {
+    return (
+      <div className='flex items-center justify-between p-4'>
+        {/* SEARCH BAR */}
+        <div className="hidden md:flex">
+          <Image src="/search.png" alt="" width={14} height={14}/>
+          <input type="text" placeholder="Search..." />
+        </div>
+        {/* ICONS & USER */}
+        <div className="">
+          <div className="bg-whit rounded-full w-7 h-7 flex items-center justify-center">
+            <Image src="/message.png" alt="" width={20} height={20} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Navbar
